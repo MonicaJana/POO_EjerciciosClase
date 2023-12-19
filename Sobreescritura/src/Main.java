879a83aa58c3478c609cd73877c686363990842d
Mónica Jaña <133398057+MonicaJana@users.noreply.github.com>
2023-12-19 10:01:30 -0500

    Sobreescritura

---
+++ Sobreescritura/src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+            Animal animal = new Animal();
+            Perro perro = new Perro();
+            Gato gato= new Gato();
+            animal.hacerSonido(); // Imprime "Haciendo sonido genérico"
+            perro.hacerSonido(); // Imprime "Gua, gua"
+            gato.hacerSonido();
+
+    }
+}
